c383750ee01c002bb6523d3dd06d4f4707fdb692
jeongyy <jeongyy@gmail.com>
2019-05-10 19:27:42 +0900

    Vuetify, Vue Router and Initial Components

---
+++ resources/js/Router/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    //{ path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+    routes // `routes: routes`의 줄임
+})
+
+export default router
